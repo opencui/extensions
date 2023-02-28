7929f57c39706c6bde80152549d0aa9911ba259f
xiaoyun wu <xiaoyun.wu@gmail.com>
2023-02-28 03:16:12 +0000

    Update CMDDispatcher.kt
    Signed-off-by: xiaoyun wu <xiaoyun.wu@gmail.com>

--- commandline/src/main/kotlin/io/opencui/dispatcher/CMDDispatcher.kt
+++ commandline/src/main/kotlin/io/opencui/dispatcher/CMDDispatcher.kt
@@ -15,77 +15,12 @@ import io.opencui.sessionmanager.*
 import java.io.File
 import java.util.logging.Logger
 
-data class CMDChannel(override val info: Configuration?) : IChannel {
-	override fun getProfile(botInfo: BotInfo, id: String): IUserIdentifier? {
-		return UserInfo("cmd", id, "master")
-	}
-
-	override fun sendWhitePayload(
-		id: String,
-		rawMessage: IWhitePayload,
-		botInfo: BotInfo,
-		source: IUserIdentifier?
-	): IChannel.Status {
-		println(rawMessage.toString())
-		return IChannel.Status("everything is good.")
-	}
-
-	override fun sendRawPayload(
-		uid: String,
-		rawMessage: JsonObject,
-		botInfo: BotInfo,
-		source: IUserIdentifier?
-	): IChannel.Status {
-		println(rawMessage.toPrettyString())
-		return IChannel.Status("everything is good.")
-	}
-
-	companion object : ExtensionBuilder<IChannel> {
-        private val logger = Logger.getLogger(CMDChannel::class.java.name)
-        private const val EXCEPTION_WAS_THROWN = "an exception was thrown"
-        private const val CLIETTOKEN = "client_token"
-        private const val CREDENTIAL = "credential"
-
-        val channelType = "cmd"
-
-        override fun invoke(config: Configuration): IChannel {
-            return CMDChannel(config)
-        }
-    }
-}
-
 
 /**
  * This dispatcher is designed to be used for debugging the chatbot in the commandline.
  */
-class CMDDispatcher(
-	val botPrefix: String,
-	val duDuckling: String = "http://127.0.0.1:8000/parse",
-	val duHost: String =  "127.0.0.1",
-	val duPort: Int = 8501,
-	val duProtocol: String = "http"
-) {
+class CMDDispatcher {
 
-	init {
-		ObjectMapper().registerModule(KotlinModule())
-		Dispatcher.botPrefix = botPrefix
-
-		val botInfo = master()
-
-		RuntimeConfig.put(DucklingRecognizer::class, duDuckling)
-		RuntimeConfig.put(TfRestBertNLUModel::class, Triple(duHost, duPort, duProtocol))
- 		RuntimeConfig.put(ChatbotLoader::class, InMemoryBotStore(botInfo))
-
-		// TODO: for persistent, we need more extra coding and deployment of storage.
-		val sessionManager = SessionManager(InMemorySessionStore(), InMemoryBotStore(botInfo))
-
-		// This make sure that we keep the existing index if we have it.
-		// I think the dispatcher can not be used as is.
-		Dispatcher.sessionManager = sessionManager
-		Dispatcher.botPrefix = botPrefix
-		// ChatbotLoader.init(File("./jardir/"), botPrefix)
-		Dispatcher.logger.info("finish the builder initialization.")
-	}
 
 	companion object {
 		@JvmStatic
